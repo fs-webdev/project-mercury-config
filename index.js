2690d3479e649fffa89ea04e7649efdb53c51f6d
Jakob Anderson <janderson@familysearch.org>
2015-03-21 16:12:29 -0600

    added config init file

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'auth': {
+    'cookieKeys': [
+      'sessionId'
+    ] 
+  },
+  'sites': {
+    'site1': [
+      'https://site1.com',
+      'https://site1.com/page1/',
+      'https://site1.com/page2/'
+    ],
+    'site2': [
+      'https://site2.com',
+      'https://site2.com/page1/',
+      'https://site2.com/page2/'
+    ],
+    'site3': [
+      'https://site3.com',
+      'https://site3.com/page1/',
+      'https://site3.com/page2/'
+    ]
+  },
+  'rules': {
+    'standard': {
+      'timeout': '60'
+    }
+  }
+}
