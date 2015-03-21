2475652cb6a3cf35be3fa202b2466ef92fc50722
Jakob Anderson <janderson@familysearch.org>
2015-03-21 16:20:20 -0600

    modularized out the config files

---
+++ config/auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'cookieKeys': [
+    'sessionId'
+  ]
+}
---
+++ config/sites.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'site1': [
+    'https://site1.com',
+    'https://site1.com/page1/',
+    'https://site1.com/page2/'
+  ],
+  'site2': [
+    'https://site2.com',
+    'https://site2.com/page1/',
+    'https://site2.com/page2/'
+  ],
+  'site3': [
+    'https://site3.com',
+    'https://site3.com/page1/',
+    'https://site3.com/page2/'
+  ]
+}
